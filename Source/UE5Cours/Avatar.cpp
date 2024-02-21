b8a55ebc737736ac5a9916c12821eeff013f90a8
Samuel Cloutier <2130147@collegebart.ca>
2024-02-20 20:22:28 -0500

    Complete Lab Cours 5

---
+++ Source/UE5Cours/Avatar.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Avatar.h"
+
+// Sets default values
+AAvatar::AAvatar()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+float AAvatar::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	Health -= DamageAmount;
+	UE_LOG(LogTemp, Warning, TEXT("Took %f damage"), DamageAmount);
+	UE_LOG(LogTemp, Warning, TEXT("Health: %f/%f"), Health, MaxHealth);
+
+	return Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+}
+
+int AAvatar::GetPercent(float _CurrentHealth, float _MaxHealth)
+{
+	return (_CurrentHealth / _MaxHealth) * 100;
+}
+
+// Called when the game starts or when spawned
+void AAvatar::BeginPlay()
+{
+	Super::BeginPlay();
+	MaxHealth = 100;
+	Health = MaxHealth;
+
+	UE_LOG(LogTemp, Warning, TEXT("Health: %f/%f"), Health, MaxHealth);
+	UE_LOG(LogTemp, Warning, TEXT("%d percent"), GetPercent(Health, MaxHealth));
+}
+
+bool AAvatar::IsDead() const
+{
+	return Health <= 0;
+}
+
+// Called every frame
+void AAvatar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAvatar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/UE5Cours/Avatar.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Avatar.generated.h"
+
+UCLASS()
+class UE5COURS_API AAvatar : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAvatar();
+
+	UPROPERTY(BlueprintReadOnly)
+	float Health;
+
+	float MaxHealth;
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+private:
+	int GetPercent(float CurrentHealth, float MaxHealth);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	bool IsDead() const;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
